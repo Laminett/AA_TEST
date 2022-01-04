f730a4a6e22856ec90a2e4fbdfe246d20b37191d
laminate <dev.atypical@gamil.com>
2022-01-05 00:17:51 +0900

    feature: 기본 controller 설정

--- src/main/java/kr/co/yeoshin/controller/IndexController.java
+++ src/main/java/kr/co/yeoshin/controller/IndexController.java
@@ -1,13 +1,63 @@
 package kr.co.yeoshin.controller;
 
+import kr.co.yeoshin.entity.Member;
+import kr.co.yeoshin.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
-    @GetMapping({"","/"})
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"", "/"})
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody
+    String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody
+    String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody
+    String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinFrom() {
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(Member member) {
+        member.setRole("ROLE_USER");
+        String rawPassword = member.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        member.setPassword(encPassword);
+        memberRepository.save(member); // 비밀번호 암호화 하지 않으면 시큐리티로 로그인 할 수 없음.
+        return "redirect:/loginForm";
     }
 }
