4dd79a44d0ec12bd28e4f014b8783b0c2561ab50
laminate <dev.atypical@gamil.com>
2022-01-04 22:58:34 +0900

    feature: 기본설정

---
+++ src/main/java/kr/co/yeoshin/entity/MemberEntity.java
@@ -0,0 +1,22 @@
+package kr.co.yeoshin.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class MemberEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String role;
+}
