223e2e278df88c3dc31e3274d0a98f248bdd07b0
laminate <dev.atypical@gamil.com>
2022-01-04 21:38:16 +0900

    first commit

---
+++ src/main/java/kr/co/yeoshin/YeoshinApplication.java
@@ -0,0 +1,13 @@
+package kr.co.yeoshin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeoshinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeoshinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/yeoshin/YeoshinApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.yeoshin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeoshinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
